f8c376c8ee4096d4ccebb1e8625c0ff94b92985b
fanshengle <fanshengle@tusdk.com>
2019-09-20 10:45:11 +0800

    update w/ tusdk 3.2.0

---
+++ Example/TuSDKDemo/Source/DemoRootView.m
@@ -0,0 +1,126 @@
+//
+//  DemoRootView.m
+//  TuSDKDemo
+//
+//  Created by Clear Hu on 15/4/24.
+//  Copyright (c) 2015年 tusdk.com. All rights reserved.
+//
+
+#import "DemoRootView.h"
+
+#pragma mark - DemoRootView
+/**
+ *  入口控制器视图
+ */
+@interface DemoRootView()<UITableViewDelegate, UITableViewDataSource>
+{
+    // 表格视图
+    TuSDKICTableView *_tableView;
+    // 缓存标记
+    NSString *_cellIdentifier;
+}
+@end
+
+@implementation DemoRootView
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self lsqInitView];
+    }
+    return self;
+}
+- (void)lsqInitView;
+{
+    // 缓存标记
+    _cellIdentifier = [NSString stringWithFormat:@"%@", [self class]];
+    
+    // 表格视图
+    _tableView = [TuSDKICTableView tableWithHeight:self.frame.size.height];
+    _tableView.delegate = self;
+    _tableView.dataSource = self;
+    _tableView.separatorStyle = UITableViewCellSeparatorStyleSingleLine;
+    if (@available(iOS 13.0, *)) {
+          _tableView.backgroundColor = [UIColor systemBackgroundColor];
+      } else {
+          // Fallback on earlier versions
+      }
+    _tableView.rowHeight = 50;
+    _tableView.sectionHeaderHeight = 30;
+    [self addSubview:_tableView];
+}
+
+- (void)setGroup:(SampleGroup *)group;
+{
+    _group = group;
+    
+    if (_tableView) [_tableView reloadData];
+}
+
+/**
+ *  获取一个范例
+ *
+ *  @param indexPath 索引
+ *
+ *  @return 范例
+ */
+- (SampleBase *)sampleWithIndexPath:(NSIndexPath *)indexPath;
+{
+    if (!indexPath || !_group) return nil;
+    
+    GroupHeader *header = [_group headerWithIndex:indexPath.section];
+    if (!header || !header.datas || header.datas.count <= indexPath.row) return nil;
+    
+    return [header.datas objectAtIndex:indexPath.row];
+}
+#pragma mark - UITableViewDelegate
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath;
+{
+    if (!self.delegate) return;
+        
+    SampleBase *sample = [self sampleWithIndexPath:indexPath];
+    if (!sample) return;
+    
+    [self.delegate demoRootView:self selectedSample:sample withAction:demoListItemActionSelected];
+}
+#pragma mark - UITableViewDataSource
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView;
+{
+    if (!_group) return 1;
+    return _group.headers.count;
+}
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section;
+{
+    if (!_group) return 0;
+    GroupHeader *header = [_group headerWithIndex:section];
+    if (!header || !header.datas) return 0;
+    return header.datas.count;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section;
+{
+    GroupHeader *header = [_group headerWithIndex:section];
+    if (!header) return nil;
+    return header.title;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath;
+{
+    TuSDKICTableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:_cellIdentifier];
+    if (!cell) {
+        cell = [TuSDKICTableViewCell initWithReuseIdentifier:_cellIdentifier];
+        cell.textLabel.font = lsqFontSize(15);
+    }
+    
+    SampleBase *sample = [self sampleWithIndexPath:indexPath];
+    if (!sample) return cell;
+    cell.textLabel.text = [NSString stringWithFormat:@"    %@", sample.title];
+    if (@available(iOS 13.0, *)) {
+        cell.textLabel.textColor = [UIColor labelColor];
+        cell.backgroundColor = [UIColor systemBackgroundColor];
+    } else {
+        // Fallback on earlier versions
+    }
+    return cell;
+}
+@end
---
+++ Example/TuSDKDemo/TuSDKFramework.h
@@ -0,0 +1,30 @@
+//
+//  TuSDKFramework.h
+//  TuSDKDemo
+//
+//  Created by Clear Hu on 15/4/24.
+//  Copyright (c) 2015年 tusdk.com. All rights reserved.
+//
+
+/**
+ * ！！！！！！！！！！！！！！！！！！！！！！！！！特别提示信息要长！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！
+ * 关于TuSDK体积（SDK编译后仅为1.9MB）：
+ * 1,如果您不需要使用本地贴纸功能，或仅需要使用在线贴纸功能，请删除/app/TuSDK.bundle/stickers文件夹
+ * 2,如果您仅需要几款滤镜，您可以删除/app/TuSDK.bundle/textures下的*.gsce文件
+ * 3,如果您不需要使用滤镜功能，请删除/app/TuSDK.bundle/textures文件夹
+ * 4,TuSDK在线管理功能请访问：https://tutucloud.com/
+ *
+ * IOS编译Framework知识：
+ * Framework包含armv7,arm64等不同CPU的编译结果的集合；
+ * 其中每种CPU编译结果还包含Debug以及Realse两种子结果；
+ * 当集成某个Framework（假如TuSDK.Framework物理文件大小为30MB），编译成APP发布后，实际大小约为不到2MB
+ *
+ * 开发文档:https://tutucloud.com/docs/ios/image/api-docs
+ */
+
+//#import "TuSDKGeeV1.h"
+#import <TuSDKGeeV1/TuSDKGeeV1.h>
+
+//#import "TuSDKGeeV2.h"
+#import <TuSDKGeeV2/TuSDKGeeV2.h>
+
